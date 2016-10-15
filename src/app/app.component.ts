b73f98b33abebaf7bd9c1b24f6d9f2c2679ef542
sniffer <sergstwnsn@yandex.ru>
2016-10-15 19:16:13 +0300

    Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
-
-  title: string;
-
-  constructor() {
-    this.title = 'Hello world!!!';
-  }
-
-}
+export class AppComponent {}

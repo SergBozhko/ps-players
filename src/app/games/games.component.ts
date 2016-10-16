640d6e689d3421120ccad8b8bac2e1432f17de48
sniffer <sergstwnsn@yandex.ru>
2016-10-16 14:20:46 +0300

    Added IGame interface

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IGame } from '../models/games.model';
+import { Game } from '../models/games.model';
+
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
@@ -8,15 +11,27 @@ import { Component, OnInit } from '@angular/core';
 
 export class GamesComponent implements OnInit {
 
-  games: string[];
+  games: IGame[];
 
   constructor() {
-    this.games = ['TES Online', 'Battlefield 1', 'GTA 5'];
+    this.games = [
+      {
+        title: 'GTA 5',
+        description: 'Cool game!',
+        price: 3500,
+        rate: 5
+      }
+    ];
   }
 
-  addGame(game: string) {
+  addGame(title: string) {
+
+    let game = new Game(title, 'Test', 2500, 4);
+
     this.games.push(game);
+
     console.log('Game added');
+
   }
 
   ngOnInit() {}

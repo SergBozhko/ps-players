e3fd2c716d62e3a7df6b94213cb8a0fb810ff137
sniffer <sergstwnsn@yandex.ru>
2016-10-17 22:05:27 +0300

    Last test changes for Roman

--- src/app/models/games.model.ts
+++ src/app/models/games.model.ts
@@ -1,25 +1,18 @@
 /**
  * Created by sniffer on 16.10.16.
  */
-export interface IGame {
-  title: string;
-  description: string;
-  price: number;
-  rate: number;
-}
 
-export class Game implements IGame {
+export class Game {
+  public title: string;
+  public description: string;
+  public price: number;
+  public rate: number;
 
-  title: string;
-  description: string;
-  price: number;
-  rate: number;
-
-  constructor(title: string, description: string, price: number, rate: number) {
-    this.title = title || 'Untitled';
-    this.description = description || '';
-    this.price = price || 0;
-    this.rate = rate || 0;
-  }
+  // constructor(title: string, description?: string, price?: number, rate?: number) {
+  //   this.title = title || 'Untitled';
+  //   this.description = description || '';
+  //   this.price = price || 0;
+  //   this.rate = rate || 0;
+  // }
 
 }

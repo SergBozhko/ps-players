8ac5d72b7c658b3f5c8f84fba3c122ca14c82d9c
sniffer <sergstwnsn@yandex.ru>
2016-10-19 21:40:21 +0300

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GamesComponent } from './games/games.component';
@@ -18,7 +19,18 @@ import {RestService} from "./shared/rest.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/games',
+        pathMatch: 'full'
+      },
+      {
+        path: 'games',
+        component: GamesComponent
+      }
+    ])
   ],
   providers: [RestService],
   bootstrap: [AppComponent]
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,15 +2,7 @@
  * Created by sniffer on 16.10.16.
  */
 
-export interface IUser {
-  name: string;
-  secondName: string;
-  nickName: string;
-  age: number;
-  platforms: string[];
-}
-
-export class User implements IUser {
+export class User {
   name: string;
   secondName: string;
   nickName: string;
